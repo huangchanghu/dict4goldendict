0ce73833967fd167eefa5ccc3ab733897ced2244
huangchanghu <changhuhuang@gmail.com>
2013-11-17 17:45:25 +0800

    ADD: add youdao online dict for goldenDict

---
+++ youdao.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+from pyquery import PyQuery as pyq
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+
+def translate():
+    url = r'http://dict.youdao.com/search?q=' + sys.argv[1]
+    doc = pyq(url)
+    doc('#custheme').remove()
+    doc('.c-topbar').remove()
+    doc('.c-subtopbar').remove()
+    doc('.c-header').remove()
+    doc('#c_footer').remove()
+    doc('.c-bsearch').remove()
+    doc('#ads').remove()
+    doc('#rel-search').remove()
+    doc('.error-wrapper').remove()
+    doc('#topImgAd').remove()
+    doc('#container').css('margin', '0')
+    doc('#results-contents').css('width', '510px')
+    doc('#results-contents').css('margin', '0')
+    doc('#result_navigator').css('left', '550px')
+    for a in doc('a'):
+        href = a.get('href')
+        if href is not None and href.startswith('/'):
+            a.make_links_absolute()
+
+    link = u"<a href='" + url + u"'>在浏览器中查看翻译</a>"
+    doc('#results-contents').append(link)
+    print doc.outerHtml()
+
+
+if __name__ == "__main__":
+    translate()
+    sys.exit(0)
---
+++ youdao_server.py
@@ -0,0 +1,47 @@
+import web
+from pyquery import PyQuery as pyq
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+# -*- coding: utf-8 -*-
+
+urls = (
+    '/search', 'search',
+    '/.*', 'search'
+)
+
+
+class search:
+    def GET(self):
+        path = web.ctx.fullpath
+        url = r'http://dict.youdao.com' + path
+        doc = pyq(url)
+        doc('#custheme').remove()
+        doc('.c-topbar').remove()
+        doc('.c-subtopbar').remove()
+        doc('.c-header').remove()
+        doc('#c_footer').remove()
+        doc('.c-bsearch').remove()
+        doc('#ads').remove()
+        doc('#rel-search').remove()
+        doc('.error-wrapper').remove()
+        doc('#topImgAd').remove()
+        doc('#container').css('margin', '0')
+        doc('#results-contents').css('width', '500px')
+        doc('#results-contents').css('margin', '0')
+        doc('#result_navigator').css('left', '550px')
+        web.header('Content-Type','text/html; charset=utf-8', unique=True)
+        return doc.outerHtml()
+
+
+class index:
+    def GET(self):
+        path = web.ctx.fullpath
+        raise web.redirect(r'http://dict.youdao.com' + path)
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.internalerror = web.debugerror
+    app.run()
